9689f8832f896605d2ce0e509a8bde0d833c49ef
Mason-King466 <mason466king@gmail.com>
2025-11-29 12:47:34 -0800

    Update main.java

--- main.java
+++ main.java
@@ -4,23 +4,34 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class main{
-  BST Users;
-  Hashtable UserNames;
-  HashTable Passwords;
-  Graph UserConnections;
-  ArrayList BST SharedInterest;
-  int numUsers;
+	BST Users;
+	ArrayList<User> UserByIndex;
+  	Hashtable UserNames;
+  	HashTable Passwords;
+  	Graph UserConnections;
+  	ArrayList BST SharedInterest;
+  	int numUsers;
+  	
+  	
+  	
+  	
+  	
+  	
+  	
+	private void makeGraph() {
+		userConnections = new Graph(UserByIndex.size() - 1);
+		User current;
+		for (int i = 1; i <= userConnections.getNumVertices(); i++) {
+			current = UserByIndex.get(i);
+			for(int j = 0; j < current.getFriendIds().getLength(); j++) {
+				current.getFriendIds().advanceIteratorToIndex(j);
+				userConnections.addDirectedEdge(i, current.getFriendIds().getIterator());
+			}
+		}
+	}
   
   
-  
-  
-  
-  
-  
-  
-  
-  
-  private void MakeFile() {
+  	private void MakeFile() {
 		File file = new File("UserData.txt");
 		try (PrintWriter writer = new PrintWriter(file)) {
 	        for (int i = 0; i < numUsers; i++) {
