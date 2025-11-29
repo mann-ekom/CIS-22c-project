e64045751a1dbe7420462afc89c72f6edf03bac4
Ariel E <156161893+arieler@users.noreply.github.com>
2025-11-29 11:17:18 -0800

    Update Interest.java

--- Interest.java
+++ Interest.java
@@ -1,1 +1,29 @@
+public class Interest {
+    private int id;        // index in ArrayList<BST<User>>
+    private String label;  // e.g. "coding", "swimming"
 
+    public Interest(int id, String label) {
+        this.id = id;
+        this.label = label;
+    }
+
+    public int getId() { return id; }
+    public String getLabel() { return label; }
+
+    @Override
+    public int hashCode() {
+        return label.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Interest)) return false;
+        Interest other = (Interest) obj;
+        return this.label.equalsIgnoreCase(other.label);
+    }
+
+    @Override
+    public String toString() {
+        return label;
+    }
+}
