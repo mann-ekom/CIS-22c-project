1a34fed47becfb761db20b17414763a034c5f645
Mason-King466 <mason466king@gmail.com>
2025-11-29 11:01:40 -0800

    Update main.java

--- main.java
+++ main.java
@@ -4,6 +4,26 @@ public class main{
   HashTable Passwords;
   Graph UserConnections;
   ArrayList BST SharedInterest;
+  int numUsers;
   
-
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  private void MakeFile() {
+		File file = new File("UserData.txt");
+		try (PrintWriter writer = new PrintWriter(file)) {
+	        for (int i = 0; i < numUsers; i++) {
+	        	writer.println(User.toString());
+	        }
+	    } catch (FileNotFoundException e) {
+	        e.printStackTrace();
+	    }
+		
+	}
 }
